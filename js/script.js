d9409178c69a9859bd3b514881cc89d40490db47
DannySilv <danielesilvestre@outlook.it>
2022-05-02 20:45:27 +0200

    Completed Bonus

--- js/script.js
+++ js/script.js
@@ -120,7 +120,7 @@ const icons = [
 	},
 ];
 
-// MILESTONE 1+2
+// MILESTONE 1+2 (FUNCTION "CARD-GENERATOR" DONE AFTER THIS, THAT CAN BE USED AS WELL INSTEAD OF THIS)
 const iconContainer = document.getElementById("icon-container")
 
 icons.forEach((element) => {
@@ -132,3 +132,55 @@ icons.forEach((element) => {
 		</button>
 	</div>`
 })
+
+// BONUS
+const type = document.getElementById("type");
+
+type.addEventListener("click", function() {
+	iconContainer.innerHTML = ''
+
+	if (type.value == "animal") {
+		const animals = icons.filter((element) => {
+			if (element.type == "animal") {
+				return true;
+			}
+		});
+		genCard(animals);
+	} else if (type.value == "vegetable") {
+		const vegetables = icons.filter((element) => {
+			if (element.type == "vegetable") {
+				return true;
+			}
+		});
+		genCard(vegetables);
+	} else if (type.value == "user") {
+		const users = icons.filter((element) => {
+			if (element.type == "user") {
+				return true;
+			}
+		});
+		genCard(users);
+	} else if (type.value == "all") {
+		genCard(icons);
+	}
+})
+
+// FUNCTION - ICON CARD GENERATOR
+/**
+ * Description -> It creates the HTML element for the icon card
+ * @param {any} array -> The array for which we are creating cards
+ * @returns {any}
+ */
+function genCard(array) {
+	const iconContainer = document.getElementById("icon-container")
+
+	array.forEach((element) => {
+		iconContainer.innerHTML += 
+		`<div class="col-lg-3 col-md-4 col-sm-6 text-center pt-2">
+			<button type="button" class="btn btn-outline-light">
+				<i class="${element.family} ${element.prefix}${element.name} ${element.color}"></i>
+				<h5>${element.name.substring(0).toUpperCase()}</h5>
+			</button>
+		</div>`
+	})
+}
